58a306121642515d6a7dfd20fe6d3177a54192ff
gitsrini <srinijammula5@gmail.com>
2021-05-08 19:47:47 +0530

    new branch created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,32 +9,41 @@ export class AppComponent {
   post: blog[]=[
     {
       imgPath:"https://images.unsplash.com/photo-1613320433989-35fc3877bb1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-      title:"Wine"
+      title:"Wine",
+      price:1500
     },
     {
       imgPath:"https://images.unsplash.com/photo-1485962232502-f25e5874a8d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-      title:"Berries"
+      title:"Berries",
+      price:2000
     },
     {
       imgPath:"https://images.unsplash.com/photo-1572451479139-6a308211d8be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-      title:"Cupcake"
+      title:"Cupcake",
+      price:500
     },
     {
       imgPath:"https://images.unsplash.com/photo-1507750549272-e58742b1df80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-      title:"Ice-cream"
+      title:"Ice-cream",
+      price:500
     },
     {
       imgPath:"https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-      title:"Pizza"
+      title:"Pizza",
+      price:750
     },
     {
       imgPath:"https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-      title:"Coffee"
+      title:"Coffee",
+      price:1700
     }
   ]
-
+  count:number=0
   recieve=[];
   getCount(title){
+    {
     this.recieve.push(title)
+    this.count++;
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { ProductComponent } from './product/product.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 
 @NgModule({
@@ -13,7 +17,11 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     UserListComponent,
     ProductComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
